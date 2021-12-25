92c38cf1d18e816cf241d8bcbbee59225d8842cb
Abdallah Abis <abis.abdallah@gmail.com>
2021-12-25 23:08:41 +0100

    add Typography component.

---
+++ packages/gatsby-theme-exo/src/components/Typography/types.ts
@@ -0,0 +1,14 @@
+import { TextProps as ThemeTextProps, ThemeUIStyleObject } from 'theme-ui';
+import DividerProps from '@exoTheme/components/Divider/types';
+
+interface TypographyLine extends DividerProps {
+  align?: 'top' | 'bottom';
+}
+
+interface TypographyPropsTypes extends ThemeTextProps {
+  sx?: ThemeUIStyleObject;
+  withLine?: boolean;
+  line?: TypographyLine;
+}
+
+export default TypographyPropsTypes;
