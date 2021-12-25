e9570677b6b55865b23cee4049be7f63edbfbb9f
Abdallah Abis <abis.abdallah@gmail.com>
2021-12-25 23:31:13 +0100

    Add useBoundingClientRectRef Hook.

---
+++ packages/gatsby-theme-exo/src/customTypes/index.ts
@@ -0,0 +1,11 @@
+export interface BoundingClientRecType {
+  exist?: boolean;
+  x: number;
+  y: number;
+  left: number;
+  top: number;
+  right: number;
+  bottom: number;
+  width: number;
+  height: number;
+}
