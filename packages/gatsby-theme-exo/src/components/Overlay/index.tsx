1b182357b3a6d5f18e4ed7b28f18e7066e865115
Abdallah Abis <abis.abdallah@gmail.com>
2022-01-03 21:56:01 +0100

    update overlay to support break points using array, and radial gradient.

--- packages/gatsby-theme-exo/src/components/Overlay/index.tsx
+++ packages/gatsby-theme-exo/src/components/Overlay/index.tsx
@@ -1,20 +1,41 @@
 import * as React from 'react';
 import { Box } from 'theme-ui';
-import OverlayProps from '@exoTheme/components/Overlay/types';
+import OverlayProps, { ColorsProps } from '@exoTheme/components/Overlay/types';
 import { keyframes } from '@emotion/react';
 
 const fadeIn = keyframes({ from: { opacity: 0 }, to: { opacity: 1 } });
 
 const Overlay: React.FC<OverlayProps> = ({
+  children,
   visible,
   zIndex,
   position,
-  image,
+  colors,
   color,
   transitionDuration,
   animated,
   ...props
 }) => {
+  const bgImage = colors?.map((color: ColorsProps) =>
+    color.linear
+      ? `linear-gradient(${
+          color.direction ? `${color.direction},` : ''
+        } ${color.linear?.join(', ')})`
+      : `radial-gradient(${
+          color.direction ? `${color.direction},` : ''
+        } ${color.radial?.join(', ')})`
+  );
+
+  // for (let i = 0; i < colors.length; i++) {
+  //   const item = colors[i];
+  //   if (Array.isArray(item)) {
+  //     if (!directions[i]) adjustedDirections.push(directions[0])
+  //     bgImage.push(`linear-gradient(${directions[0] || directions[i - 1]}, ${item?.join(', ')})`)
+  //   } else {
+  //     bgImage.push(`linear-gradient(${directions}, ${colors?.join(', ')})`)
+  //   }
+  // }
+
   return visible ? (
     <Box
       {...props}
@@ -24,13 +45,15 @@ const Overlay: React.FC<OverlayProps> = ({
           ? `${fadeIn} ${transitionDuration || '1000'}ms`
           : 'none',
         transition: animated ? `all ${transitionDuration || '1000'}ms` : 'none',
-        backgroundColor: !image && (color || 'grey'),
-        backgroundImage: image,
+        backgroundColor: !colors?.length && (color || 'grey'),
+        backgroundImage: bgImage,
         position: position,
         inset: 0,
         zIndex: zIndex
       }}
-    />
+    >
+      {children}
+    </Box>
   ) : null;
 };
 
--- packages/gatsby-theme-exo/src/components/Overlay/types.ts
+++ packages/gatsby-theme-exo/src/components/Overlay/types.ts
@@ -1,13 +1,25 @@
 import { BoxProps } from 'theme-ui';
 
 interface OverlayProps extends BoxProps {
-  image?: string;
   color?: string;
   visible?: boolean;
   zIndex?: string | number;
   position?: 'fixed' | 'absolute';
   transitionDuration?: number;
   animated?: boolean;
+  colors?: ColorsProps[];
 }
+
+export type ColorsProps =
+  | {
+      direction?: string;
+      linear: NonEmptyArray<string> | NonEmptyArray<NonEmptyArray<string>>;
+      radial?: never;
+    }
+  | {
+      direction?: string;
+      linear?: never;
+      radial: NonEmptyArray<string> | NonEmptyArray<NonEmptyArray<string>>;
+    };
 
 export default OverlayProps;
