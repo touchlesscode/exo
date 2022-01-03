f2a0793f2470a63f3d18c859ab9086fbfde6530b
Abdallah Abis <abis.abdallah@gmail.com>
2022-01-03 22:07:43 +0100

    update shadows.

--- packages/gatsby-theme-exo/src/theme/shadows.ts
+++ packages/gatsby-theme-exo/src/theme/shadows.ts
@@ -1,16 +1,16 @@
 const shadows = {
-  xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
-  sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-  default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-  md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-  lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-  xl: `0 20px 25px -5px rgba(0, 0, 0, 0.1),
-      0 10px 10px -5px rgba(0, 0, 0, 0.04)`,
-  '2xl': '0 0px 150px 20px rgba(0, 0, 0, 0.25)',
+  0: 'none',
+  1: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+  2: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+  3: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+  4: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+  5: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+  6: '0 25px 50px -12px rgb(0 0 0 / 0.25)',
+  7: '0 30px 80px 20px rgba(0, 0, 0, 0.15)',
+  8: '0px 4px 80px 0px rgba(0,0,0,0.25), 0px -4px 80px -31px rgba(0,0,0,0.25)',
+  9: '0px 8px 24px 0px rgba(84, 84, 84, 0.26)',
   inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-  outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
-  'full-md': '0px 8px 24px 10px rgba(84, 84, 84, 0.26)',
-  none: 'none'
+  outline: '0 0 0 3px rgba(66, 153, 225, 0.5)'
 };
 
 export default shadows;
