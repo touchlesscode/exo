0ec88c2a78e66d120d13546b19a3686e61696bb0
Touchless Inc <info@touchlessinc.com>
2022-03-23 15:38:09 -0400

    remove extra

--- packages/gatsby-theme-exo/src/pages/widget/textkit/onboarding/index.tsx
+++ packages/gatsby-theme-exo/src/pages/widget/textkit/onboarding/index.tsx
@@ -1,53 +1,15 @@
 import React, { useContext, useEffect, useState, useReducer } from 'react';
-import Loading from '@exo/components/Loading';
-import AddButton from '@exo/components/AddButton';
-import { UserContext } from '@exo/context/UserContext';
-import { MainWrapper } from '@exo/components/Wrappers';
-import { ThemeProvider } from 'styled-components'
-import { WidgetType, WidgetViewSize, WidgetEvents } from '@exo/hooks/widgetApi';
-import axios from 'axios';
-import { useFormik } from 'formik';
-import Dropdown, { Option } from '@exo/components/Dropdown';
-import TextInput from '@exo/components/TextInput';
-import { createOptions, validationAddress, validationPrimary } from '@exo/utils';
-import { GlobalStyle, defaultTheme as theme } from '@exo/theme'
-import useWidgetReady from '@exo/hooks/useWidgetReady';
-import useTextKitContext from '@exo/hooks/useTextKitContext';
+import { TextKitWidgetProvider } from '@statflo/textkit-widget-sdk-react';
+import Widget from './Widget';
 
-const OnBoardingPage = () => {
+
+const SampleWidget = () => {
      
-     const ready = useWidgetReady({
-        type: WidgetType.Default,
-        label: "",
-        header: "Details",
-        footer: ""
-     });
-
-     const { postEvent, event } = useTextKitContext(UserContext);
-     const { error, data, loading, reloadContact } = useTextKitContext();
-     const [init, setInit] = useState<boolean>(false);
-     const [state, setState] = useState<TState>('compact');
-     const { expanded } = useContext(UserContext);
-     
-    useEffect(() => {
-        if (!init && ready) {
-            setInit(true);
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [init, ready]);
-
-    useEffect(() => {
-        if (!expanded) {
-            setState('compact');
-        }
-
-        if (expanded && state !== 'form' && state !== 'view') {
-            setState('list');
-        }
-    }, [expanded, state]);
-
-        
-    return <div>Index Page</div>;
+    return (
+        <TextKitWidgetProvider header='Standard Widget' footer='Show More'>
+            <Widget />
+        </TextKitWidgetProvider>
+    );
 };
 
-export default OnBoardingPage;
+export default SampleWidget;
