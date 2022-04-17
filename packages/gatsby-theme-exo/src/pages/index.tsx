b8c6db279aeb2ea6d8363fd14bfe64f286bb3edc
Abdallah Abis <abis.abdallah@gmail.com>
2022-04-17 21:57:10 +0000

    feat: consume the shared components library on the theme

--- packages/gatsby-theme-exo/src/pages/index.tsx
+++ packages/gatsby-theme-exo/src/pages/index.tsx
@@ -1,5 +1,16 @@
-const index = () => {
-  return <h1>Hello from gatsby-theme-exo</h1>
+import { Button } from '@touchless/react'
+import React from 'react'
+
+interface PageProperties {
+  name?: string
+}
+const Page = ({ name = 'Exo Theme' }: PageProperties) => {
+  return (
+    <div>
+      <h1>Hello from gatsby-theme-exo</h1>
+      <Button type='button'>{name}</Button>
+    </div>
+  )
 }
 
-export default index
+export default Page
